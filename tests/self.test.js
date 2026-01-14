36a103793c5262a89b764c5f5191558b8d6bd45a
Perki <perki@pryv.com>
2026-01-14 14:21:29 +0100

    Cleaning up tests

--- tests/self.test.js
+++ tests/self.test.js
@@ -1,20 +1,12 @@
 /* eslint-env mocha */
 const assert = require('node:assert/strict');
-const { init: initTestServer, apiTest, partnerAuth, createOnboardedUser } = require('./helpers/testServer');
-const ShortUniqueId = require('short-unique-id');
-const { getConfig } = require('boiler');
+const { init: initTestServer, apiTest, createOnboardedUser } = require('./helpers/testServer');
 
 describe('[SELX] SELF', function () {
   this.timeout(5000);
-  const testRnd = (new ShortUniqueId({ dictionary: 'alphanum_lower', length: 8 })).rnd();
-  let mainStreamId = null;
 
   before(async () => {
     await initTestServer();
-    // get the main streamId for the userPermissionRequest servic
-    const config = await getConfig();
-    const firsStream = config.get('service:userPermissionRequest')[0];
-    mainStreamId = firsStream.streamId;
   });
 
   it('[SELA] GET /self/status', async () => {
